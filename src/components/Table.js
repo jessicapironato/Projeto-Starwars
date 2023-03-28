456b8e6f943fb101d7fa762755a211bae24262b2
Jéssica <jessicapmaximo@gmail.com>
2023-03-28 18:59:18 -0300

    requisito 2 ok

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,46 +2,60 @@ import React, { useContext } from 'react';
 import AppContext from '../contexts/AppContext';
 
 export default function Table() {
-  const { planets } = useContext(AppContext);
-  console.log(planets);
+  const { planets, searchPlanetName, handleSearchPlanetName } = useContext(AppContext);
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>Url</th>
-        </tr>
-      </thead>
-      <tbody>
-        {planets.map((planet, index) => (
-          <tr key={ index }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
-          </tr>))}
-      </tbody>
-    </table>
+    <>
+      <h1>Project Starwars Planets Search</h1>
+      <label htmlFor="name">
+        Name
+        <input
+          data-testid="name-filter"
+          type="text"
+          name="name"
+          value={ searchPlanetName }
+          onChange={ handleSearchPlanetName }
+        />
+
+      </label>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>Url</th>
+          </tr>
+        </thead>
+        <tbody>
+          {planets.map((planet, index) => (
+            <tr key={ index }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>))}
+        </tbody>
+      </table>
+    </>
 
   );
 }
--- src/contexts/AppProvider.jsx
+++ src/contexts/AppProvider.jsx
@@ -5,7 +5,10 @@ import AppContext from './AppContext';
 
 export default function AppProvider({ children }) {
   const [planets, setPlanets] = useState([]);
+  const [searchPlanetName, setSearchPlanetName] = useState('');
+  const [planetsFixed, setPlanetsFixed] = useState([]);
 
+  // requisito 1
   const fetchData = async () => {
     const request = await fetch('https://swapi.dev/api/planets');
     const response = await request.json();
@@ -15,15 +18,28 @@ export default function AppProvider({ children }) {
       return element;
     });
     setPlanets(resultPlanet);
+    setPlanetsFixed(resultPlanet);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  // requisito 2 Realizado com auxílio do Filipe Santana na monitoria. Foi necessário criar o setPlanetsFixed para realizar o filter. Se não fizesse dessa forma, após limpar a digitação, os planetas estariam apagados, ou seja, estava criando um resultado, apagando os resultados originais. o setPlanetFixed preserva as informações originais
+
+  const handleSearchPlanetName = ({ target }) => {
+    setSearchPlanetName(target.value);
+  };
+  useEffect(() => {
+    const searchPlanets = planetsFixed.filter((planet) => planet
+      .name.includes((searchPlanetName)));
+    setPlanets(searchPlanets);
+    console.log(searchPlanets);
+  }, [searchPlanetName, planetsFixed]);
+
   return (
 
-    <AppContext.Provider value={ { planets } }>
+    <AppContext.Provider value={ { planets, handleSearchPlanetName } }>
       { children }
     </AppContext.Provider>
 
